ddb8854865518834f87b2e849e5619026babfc7a
Baljinder singh <123756514+baljinder0630@users.noreply.github.com>
2023-12-18 16:24:27 +0530

    Update location.js

--- routes/location.js
+++ routes/location.js
@@ -3,7 +3,7 @@ import getLocation from "../controller/location.js/getLocation.js"
 import updateLocation from "../controller/location.js/updateLocation.js"
 const router = express.Router()
 
-router.post("/patient",getLocation)
+router.get("/patient",getLocation)
 router.post("/update",updateLocation)
 
 export default router
