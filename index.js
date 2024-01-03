70f0eeaedf2cf965209367a2f5c0f554338086a9
Baljinder singh <baljinder@medlr.in>
2024-01-04 00:40:17 +0530

    Working with socket io

--- index.js
+++ index.js
@@ -2,22 +2,45 @@ import express from "express"
 import dotenv from 'dotenv'
 import api from "./routes/api.routes.js"
 import db from "./config/db.js"
+import http from 'http'
+import { Server } from 'socket.io'
+
+
 
 dotenv.config()
 const app = express()
-
+const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.json())
-
 app.use('/api', api)
-
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send('Socket.io server is running.');
+});
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection');
+
+    // Emit the 'message' event to the connected socket
+    socket.emit('message', 'Welcome!');
+
+    // Listen for 'msg' event from the client
+    socket.on('msg', (data) => {
+        console.log('Message from client:', data);
+    });
+});
+
+
+app.get('/health', (req, res) => {
+    return res.status(200).send("Health OK")
 })
 
 const port = process.env.PORT
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("Listening to the port " + port)
 })
 
 export default app
+
+
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,12 @@ import signup from "../controller/auth/signup.js"
 import signin from "../controller/auth/signin.js"
 import verifyToken from "../controller/auth/verifyToken.js"
 import refreshToken from "../controller/auth/refreshToken.js"
+import logout from "../controller/auth/logout.js"
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/signin", signin)
+router.post("/logout", logout)
 router.post("/verifyToken", verifyToken)
 router.post("/refreshToken", refreshToken)
 
